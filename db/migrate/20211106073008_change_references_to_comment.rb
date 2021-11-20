22741f78aaedf2643727c67d78f9465f4f4d5d46
taka <iloveruby8989@gmail.com>
2021-11-20 03:31:21 +0000

    RemoveVideo

--- db/migrate/20211106073008_change_references_to_comment.rb
+++ db/migrate/20211106073008_change_references_to_comment.rb
@@ -1,7 +1,5 @@
 class ChangeReferencesToComment < ActiveRecord::Migration[6.0]
   def change
-    remove_column :comments, :video_id
     add_reference :comments, :user
-    add_reference :comments, :video
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -25,9 +25,7 @@ ActiveRecord::Schema.define(version: 2021_11_10_120647) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
-    t.integer "video_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
-    t.index ["video_id"], name: "index_comments_on_video_id"
   end
 
   create_table "s3files", force: :cascade do |t|
