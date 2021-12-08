e40949af7313d0cbcc4f0f349c8736a43108c61e
taka <iloveruby8989@gmail.com>
2021-12-08 16:54:40 +0000

    Fix Guests-login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,32 +1,32 @@
 module SessionsHelper
   def current_user
-    return unless (user_id = cookies[:user_id])
+    return unless (user_id = session[:user_id])
 
     @current_user ||= User.find_by(id: user_id)
   end
   
   def guest_log_in
-    if cookies[:user_id]
-      user = User.find_by(id: cookies[:user_id]) 
+    if session[:user_id]
+      @user = User.find_by(id: session[:user_id]) 
     else
-      user = User.create
-      user.update_attributes(
-        name: "ゲスト#{user.id}",
-        email: "guest#{user.id}@example.com",
+      @user = User.create
+      @user.update(
+        name: "ゲスト#{@user.id}",
+        email: "guest#{@user.id}@example.com",
         password: SecureRandom.urlsafe_base64
       )
-      channel = user.channels.build(name: user.name)
+      channel = @user.channels.build(name: @user.name)
       channel.save
     end
-    log_in user
+    log_in @user
   end
   
   def log_in(user)
-    cookies.permanent.signed[:user_id] = user.id
+    session[:user_id] = user.id
   end
 
   def log_out
-    cookies.delete :user_id
+    session.delete(:user_id)
     @current_user = nil
   end
 end
