a84f33f8fd3c17db4c2445752af73108c038920a
taka <iloveruby8989@gmail.com>
2021-12-20 15:17:47 +0000

    Get User Instance in log_in Method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,6 @@
 module SessionsHelper
   def current_user
     return unless (user_id = session[:user_id])
-
     @current_user ||= User.find_by(id: user_id)
   end
   
@@ -20,13 +19,15 @@ module SessionsHelper
     end
     log_in @user
   end
-  
-  def log_in(user)
-    session[:user_id] = @user.id
-  end
 
+  def log_in(user)
+    session[:user_id] = user.id
+    @current_user = user
+  end
+  
   def log_out
     session.delete(:user_id)
     @current_user = nil
   end
+  
 end
